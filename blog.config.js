a07aead4baa4728d15d8b50bd4a79f036ce15a12
zoe bai <zhiyub@alumni.cmu.edu>
2021-09-03 19:42:43 -0700

    a

--- blog.config.js
+++ blog.config.js
@@ -32,7 +32,7 @@ const BLOG = {
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: 'G-T6VHWQ6ZJ8' // e.g: G-XXXXXXXXXX
+      measurementId: '' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
